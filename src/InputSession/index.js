a5efb4999fe465122d5ba4d4ff63b1e73239e73c
511234 <tungkl1200@gmail.com>
2022-02-13 14:17:53 +0800

    Clean code

--- src/InputSession/index.js
+++ src/InputSession/index.js
@@ -1,9 +1,9 @@
 import React, { useContext, useState } from "react"
 
-import { Paper, Divider, TextField, Button } from "@mui/material"
+import { Paper, InputLabel, TextField, Button } from "@mui/material"
 import SendIcon from "@mui/icons-material/Send"
 import "./index.css"
-import { EDIT_SHEET_ID, EDIT_SHEET_NAME, EntryContext } from "../Hooks"
+import { EDIT_SHEET_ID, EntryContext } from "../Hooks"
 
 export const InputSession = () => {
   const entryContext = useContext(EntryContext)
@@ -16,13 +16,7 @@ export const InputSession = () => {
   return (
     <Paper elevation={3} className="input-container">
       <h1 style={{ textAlign: "center" }}> Google Sheet Widget</h1>
-      <h3>A handly application for you to view Google Sheet and draw Random People!</h3>
-      <ul>
-        <li>Copy Sheet ID from URL</li>
-        <li>Paste it in the field</li>
-        <li>Switch between different tabs to play around!</li>
-      </ul>
-
+      {/* <InputLabel id="sheet-id">SHEET ID</InputLabel> */}
       <TextField
         fullWidth
         margin="dense"
